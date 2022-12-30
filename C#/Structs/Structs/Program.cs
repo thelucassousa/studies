be35993a25cc342d5c1f4bd28d794e858d0b0784
lucas <luccasous4@outlook.com>
2022-12-29 21:56:05 -0300

    updating day

--- C#/Structs/Structs/Program.cs
+++ C#/Structs/Structs/Program.cs
@@ -15,15 +15,30 @@ namespace Structs
             public float peso;
             public float preco;
             public string marca;
-                
+            
+
+            public Produto(string nome, float preco, float peso, string marca)
+            {
+                this.nome = nome; // o que a linha está dizendo:l variavel do atributo recebe variavel do parametro - lembrando que quando estou chamando "nome" estou me referenciando ao parametro da função e não ao atributo, lembrar do escopo.
+                this.preco = preco;//lembrando que precisa declarar todos e não somente um.
+                this.peso = peso;
+                this.marca = marca;
+            }
         }
 
 
         static void Main(string[] args)
         {
-            Produto bola = new Produto(); //chamando a estrutura;
-            bola.nome = "MyBol";
-            bola.marca = "Nike";
+
+            /*
+            Produto computador = new Produto();
+            computador.nome = "Ideaped 330s";
+            computador.marca = "Lenovo";
+            computador.peso = 1.2f;
+            computador.preco = 3999f;
+            */
+
+            Produto computador = new Produto("Ideaped 330s", 3999f, 1.2f, "Lenovo");
         }
     }
 }
